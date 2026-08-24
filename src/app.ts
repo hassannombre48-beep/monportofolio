3a8464f2bf95c1e79607209936b351d3ed4d08da
hass226 <nombrehassan@gmail.com>
2026-08-24 23:49:06 +0200

    Fix gestion des fichiers et correction EISDIR dans profile.controller securiter tout helmet nvvh

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ import experienceRoutes from "./modules/experience/experience.routes";
 const app = express();
 
 // 🔹 Middleware global
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
 
 
 applySecurity(app);
